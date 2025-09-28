9e0328a17918e54388ba6109b5a4acd037f1b90d
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-28 11:47:35 +0300

    react-web-example

---
+++ new-react-web/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './css/Header.css'
+function Header() {
+  return (
+    <div className='header'>
+<div className='header-title'>KÜTAHYA TUR REHBERİ</div>
+    </div>
+  )
+}
+
+export default Header
