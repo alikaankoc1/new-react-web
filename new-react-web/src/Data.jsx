e79ed1cc07522eba2e6013ef1a9b056967cfe180
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-28 12:04:37 +0300

    data.jsx

---
+++ new-react-web/src/Data.jsx
@@ -0,0 +1,32 @@
+import Camii from './images/cami.jpeg'
+import  Dpü from './images/dpü.jepg'
+import Kale from './images/kale.webp'
+import Şehitlik from './images/şehitlik.jepg'
+
+const travel = [{
+  id: 1,
+  title: "Kütahya Ulu Camii",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis sequi tempora omnis! Quia culpa vel in voluptate totam quae repellat!",
+  image: Camii
+},
+{
+  id: 2,
+  title: "Dumlupınar Üniversitesi",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis sequi tempora omnis! Quia culpa vel in voluptate totam quae repellat!",
+  image: Dpü
+}
+,
+{
+  id: 3,
+  title: "Kütahya Kalesi",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis sequi tempora omnis! Quia culpa vel in voluptate totam quae repellat!",
+  image: Dpü
+}
+,
+{
+  id: 4,
+  title: "Dumlupınar Şehitliği",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis sequi tempora omnis! Quia culpa vel in voluptate totam quae repellat!",
+  image: Şehitlik
+}
+]
